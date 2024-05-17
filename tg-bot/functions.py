b1e86e19813c47adbe895693f35e391574bdaccc
Chris Rodgers <crodgers@pga-WN6NKFPM6V-crodgers.hsd1.fl.comcast.net>
2024-05-16 20:18:07 -0400

    setup assistant

--- tg-bot/functions.py
+++ tg-bot/functions.py
@@ -1,88 +1,59 @@
+import os
+import requests
 from cairosvg import svg2png
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+def generate_image(prompt: str):
+    response = openai.images.generate(
+        prompt=prompt, model="dall-e-3", n=1, size="1024x1024"
+    )
+    image_url = response.data[0].url
+    image_response = requests.get(image_url)
+    return image_response
 
 def svg_to_png_bytes(svg_string):
   png_bytes = svg2png(bytestring=svg_string.encode('utf-8'))
   return png_bytes
-
-def python_math_execution(math_string):
-  try:
-    answer = eval(math_string)
-    if answer:
-      return str(answer)
-  except:
-    return 'invalid code generated'
-
-def generate_image(prompt: str):
-    if "generate an image" in prompt.lower():
-        return "Sure, I'll get right on that!"
-    else:
-        return "No suitable image type found for the prompt."
     
 def run_function(name: str, args: dict):
-  if name == "svg_to_png_bytes":
-      return svg_to_png_bytes(args["svg_string"])
-  elif name == "python_math_execution":
-      return python_math_execution(args["math_string"])
-  elif name == "generate_image":
-      return generate_image(args["prompt"])
-  else:
-      return None
+    if name == "svg_to_png_bytes":
+        return svg_to_png_bytes(args["svg_string"])
+    if name == "generate_image":
+        return generate_image(args["prompt"])
+    return None
 
 functions = [
     {
-        "type": "function",
-        "function": {
-            "name": "svg_to_png_bytes",
-            "description": "Generate a PNG from an SVG",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "svg_string": {
-                        "type":
-                        "string",
-                        "description":
-                        "A fully formed SVG element in the form of a string",
-                    },
+        "name": "svg_to_png_bytes",
+        "description": "Generate a PNG from an SVG",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "svg_string": {
+                    "type": "string",
+                    "description": "A fully formed SVG element in the form of a string",
                 },
-                "required": ["svg_string"],
             },
+            "required": ["svg_string"],
         },
     },
     {
-        "type": "function",
-        "function": {
-            "name": "python_math_execution",
-            "description": "Solve a math problem using python code",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "math_string": {
-                        "type":
-                        "string",
-                        "description":
-                        "A string that solves a math problem that conforms with python syntax that could be passed directly to an eval() function",
-                    },
-                },
-                "required": ["math_string"],
-            },
-        },
-    },
-    {
-       "type": "function",
-        "function": {
         "name": "generate_image",
-        "description": "Generates an image based on a user's prompt",
+        "description": "generate an image using the dalle api from a prompt",
         "parameters": {
             "type": "object",
             "properties": {
                 "prompt": {
                     "type": "string",
-                    "description": "A user's prompt suggesting the type of image to generate",
+                    "description": "an image generation prompt",
                 },
             },
             "required": ["prompt"],
         },
-      }
-    }
+    },
 ]
-
--- tg-bot/main.py
+++ tg-bot/main.py
@@ -1,8 +1,7 @@
 import os
+import time
 import json
 import logging
-import requests
-import replicate
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -14,34 +13,187 @@ from telegram.ext import (
 from dotenv import load_dotenv
 from openai import OpenAI
 from functions import functions, run_function
-from replicate import Client
 
 load_dotenv()
 
-openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-tg_bot_token = os.getenv("TG_BOT_TOKEN")
+CODE_PROMPT = """
+Here are two input:output examples for code generation. Please use these and follow the styling for future requests that you think are pertinent to the request.
+Make sure All HTML is generated with the JSX flavoring.
+// SAMPLE 1
+// A Blue Box with 3 yellow cirles inside of it that have a red outline
+<div style={{   backgroundColor: 'blue',
+  padding: '20px',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '300px',
+  height: '100px', }}>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+</div>
+"""
 
-messages = [{
-  "role": "system",
-  "content": "You are a helpful assistant that answers questions."
-}]
+tg_bot_token = os.getenv("TG_BOT_TOKEN")
+openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+scraped_csv_path = os.path.join(current_dir, "processed", "scraped.csv")
+
+mdn_scrape_file = openai.files.create(
+    file=open(scraped_csv_path, "rb"), purpose="assistants"
+)
+
+assistant = openai.beta.assistants.create(
+    name="Telegram Bot",
+    instructions=CODE_PROMPT,
+    tools=[
+        {"type": "code_interpreter"},
+        {"type": "file_search"},
+        {"type": "function", "function": functions[0]},
+        {"type": "function", "function": functions[1]},
+    ],
+    model="gpt-4-0125-preview",
+    tool_resources={
+      "code_interpreter": {
+        "file_ids": [mdn_scrape_file.id]
+      }
+    }
+)
+THREAD = openai.beta.threads.create()
+
+async def assistant_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = openai.beta.threads.messages.create(
+        thread_id=THREAD.id, role="user", content=update.message.text
+    )
+    run = openai.beta.threads.runs.create(
+        thread_id=THREAD.id, assistant_id=assistant.id
+    )
+    run = wait_on_run(run, THREAD)
+    # if we did a function call, run the function and update the thread's state
+    if run.status == "requires_action":
+        print(run.required_action.submit_tool_outputs.tool_calls)
+        tool_call = run.required_action.submit_tool_outputs.tool_calls[0]
+        name = tool_call.function.name
+        args = json.loads(tool_call.function.arguments)
+        response = run_function(name, args)
+        if name in ("svg_to_png_bytes"):
+            await context.bot.send_photo(
+                chat_id=update.effective_chat.id, photo=response
+            )
+        if name in ("generate_image"):
+          run = openai.beta.threads.runs.cancel(thread_id=THREAD.id, run_id=run.id)
+          run = wait_on_run(run, THREAD)
+          await context.bot.send_photo(
+              chat_id=update.effective_chat.id, photo=response.content
+          )
+          return
+        run = openai.beta.threads.runs.submit_tool_outputs(
+            thread_id=THREAD.id,
+            run_id=run.id,
+            tool_outputs=[
+                {"tool_call_id": tool_call.id, "output": json.dumps(str(response))}
+            ],
+        )
+        run = wait_on_run(run, THREAD)
+    
+    # Retrieve the message object
+    messages = openai.beta.threads.messages.list(
+        thread_id=THREAD.id, order="asc", after=message.id
+    )
+    # Extract the message content
+    message_content = messages.data[0].content[0].text
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=message_content.value
+    )
+
+def wait_on_run(run, thread):
+    while run.status in ("queued", "in_progress"):
+        print(run.status)
+        run = openai.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+async def transcribe_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Safety Check
+    voice_id = update.message.voice.file_id
+    if voice_id:
+        file = await context.bot.get_file(voice_id)
+        await file.download_to_drive(f"voice_note_{voice_id}.ogg")
+        await update.message.reply_text("Voice note downloaded, transcribing now")
+        audio_file = open(f"voice_note_{voice_id}.ogg", "rb")
+        transcript = openai.audio.transcriptions.create(
+            model="whisper-1", file=audio_file
+        )
+        message = openai.beta.threads.messages.create(
+            thread_id=THREAD.id, role="user", content=transcript.text
+        )
+        run = openai.beta.threads.runs.create(
+            thread_id=THREAD.id, assistant_id=assistant.id
+        )
+        await update.message.reply_text(
+            f"Transcript finished:\n {transcript.text}\n processing request"
+        )
+        run = wait_on_run(run, THREAD)
+        # if we did a function call, run the function and update the thread's state
+        if run.status == "requires_action":
+            print(run.required_action.submit_tool_outputs.tool_calls)
+            tool_call = run.required_action.submit_tool_outputs.tool_calls[0]
+            name = tool_call.function.name
+            args = json.loads(tool_call.function.arguments)
+            response = run_function(name, args)
+            if name in ("svg_to_png_bytes"):
+                await context.bot.send_photo(
+                    chat_id=update.effective_chat.id, photo=response
+                )
+            if name in ("generate_image"):
+                await context.bot.send_photo(
+                    chat_id=update.effective_chat.id, photo=response.content
+                )
+                run = openai.beta.threads.runs.cancel(
+                    thread_id=THREAD.id, run_id=run.id
+                )
+                run = wait_on_run(run, THREAD)
+                return
+            run = openai.beta.threads.runs.submit_tool_outputs(
+                thread_id=THREAD.id,
+                run_id=run.id,
+                tool_outputs=[
+                    {"tool_call_id": tool_call.id, "output": json.dumps(str(response))}
+                ],
+            )
+            run = wait_on_run(run, THREAD)
+        # Retrieve the message object
+        messages = openai.beta.threads.messages.list(
+            thread_id=THREAD.id, order="asc", after=message.id
+        )
+        # Extract the message content
+        message_content = messages.data[0].content[0].text
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text=message_content.value
+        )
 
 logging.basicConfig(
   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
   level=logging.INFO)
-
-replicate_token = os.getenv('REPLICATE_API_TOKEN')
-
-client = Client(api_token=replicate_token)
-model = client.models.get("meta/llama-2-70b-chat")
-version = model.versions.get("2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1")
-
-def generate_prompt(messages):
-  return "\n".join(f"[INST] {message['text']} [/INST]"
-                   if message['isUser'] else message['text']
-                   for message in messages)
-
-message_history = []
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -65,47 +217,13 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
   message_history.append({"isUser": False, "text": human_readable_output})  
 
-async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  response = openai.images.generate(prompt=update.message.text,
-                                    model="dall-e-3",
-                                    n=1,
-                                    size="1024x1024")
-  image_url = response.data[0].url
-  image_response = requests.get(image_url)
-  await context.bot.send_photo(chat_id=update.effective_chat.id,
-                               photo=image_response.content)
-
-async def sdxl(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  input = {
-      "width": 768,
-      "height": 768,
-      "prompt": update.message.text,
-      "refine": "expert_ensemble_refiner",
-      "apply_watermark": False,
-      "num_inference_steps": 25
-  }
-
-  response = replicate.run(
-      "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
-      input=input
-  )
-  print(response[0])
-  image_url = response[0]
-  image_response = requests.get(image_url)
-  await context.bot.send_photo(chat_id=update.effective_chat.id,
-                                photo=image_response.content)
-  
 if __name__ == '__main__':
   application = ApplicationBuilder().token(tg_bot_token).build()
 
   start_handler = CommandHandler('start', start)
-  chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
-  image_handler = CommandHandler('image', image)
-  sdxl_handler = CommandHandler('sdxl', sdxl)
+  chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), assistant_chat)
 
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
-  application.add_handler(image_handler)
-  application.add_handler(sdxl_handler)
 
   application.run_polling()
